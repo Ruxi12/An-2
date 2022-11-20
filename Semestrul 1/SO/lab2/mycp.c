41af81c8381def16f8bc7889aef3c48ea0f959dd
Ruxi12 <95472267+Ruxi12@users.noreply.github.com>
2022-11-20 20:41:46 +0200

    Update mycp.c

--- Semestrul 1/SO/lab2/mycp.c
+++ Semestrul 1/SO/lab2/mycp.c
@@ -1,45 +1,47 @@
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <stdio.h>
 #include <unistd.h>
 
-int main(int agrc, char* argv[]){
+int main(int argc, char* argv[]) {
     char buf[4096];
     size_t read_size;
+    // open the file specified in the pathname
+    // acces mode = O_RDONLY - opened for reading
     int fd1 = open(argv[1], O_RDONLY);
-    if(fd1 < 0){
-        perror("Could Not Open First File\n");
+    // check if opened correctly, with no error
+    if (fd1 < 0){
+        perror("Could not open first file\n");
         return errno;
     }
-
-    int fd2 = open(argv[2], O_WRONLY | O_CREAT, 0666);
-    
-    if(fd2 < 0){
-        perror("Could Not Open Second File\n");
+    // 0666 octal
+    // i.e. every one of the 6's corresponds to three permission bits
+    // -rw-rw-rw-
+    // all users have read write permissions
+    int fd2 = open (argv[2], O_WRONLY | O_CREAT, 0666);
+    if (fd2 < 0){
+        perror("Could not open second file\n");
         return errno;
     }
-
-    while(read_size = read(fd1, buf, sizeof(buf))){
-        if(read_size < 0){
-            perror("Read Failed\n");
+    // read file
+    while (read_size = read(fd1, buf, sizeof(buf))){
+        if (read_size < 0){
+            perror("Read failed\n");
             return errno;
         }
-        if(write(fd2, buf, read_size) <0){
-            perror("Write Failed\n");
+        if (write(fd2, buf, read_size) < 0){
+            perror("Write failed\n");
             return errno;
         }
     }
-
-    if(close(fd1) < 0){
-        perror("Close First File Failed\n");
+    // verify if files are closed
+    if (close(fd1) < 0){
+        perror("Close first file failed\n");
         return errno;
     }
-    if(close(fd2) < 0){
-        perror("Close Second File Failed\n");
+    if (close(fd2) < 0){
+        perror("Close second file failed \n");
         return errno;
     }
     return 0;
-
 }
